c7cf5264f16b71d81b98d6baf3ec067ccd271380
Tomasz Skraskowski <tomasz.skraskowski@pirios.com>
2020-05-05 06:24:53 +0200

    Add boilerplate code for sending emails

--- _functions/send.js
+++ _functions/send.js
@@ -1,4 +1,25 @@
 exports.handler = function (event, context, callback) {
+    console.log('------------- REQUEST STARTED -------------------- VER 2020_05_05__18_39');
+
+    console.log('event http method: ' + event.httpMethod);
+    console.log('event body: ' + event.body);
+
+    const SENDGRID_API_ENDPOINT = "https://api.sendgrid.com/v3/mail/send";
+    const SENDGRID_API_KEY = process.env.SENDGRID;
+    console.log("SendGrid api key: " + SENDGRID_API_KEY.substring(0, 4) + "...");
+
+    const MAILERLITE_API_KEY = process.env.MAILERLITE;
+    console.log("MailerLite api key: " + MAILERLITE_API_KEY.substring(0, 4) + "...");
+
+    const fetch = require("node-fetch");
+
+    const eventBody = JSON.parse(event.body);
+
+    console.log("email: " + eventBody.email);
+    console.log("agreeGitInbox: " + eventBody.agreeGitInbox);
+    console.log("agreeGitWarsztatyInbox : " + eventBody.agreeGitWarsztatyInbox);
+    console.log("camefromformlocation : " + eventBody.camefromformlocation);
+    
     try {
         try {
             // This will use SendGrid which often fails, but it's only informational for us, so we don't show failure to user
