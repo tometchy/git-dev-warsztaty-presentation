869595a56b0fe1fb98049981f0bc30590dc4ca89
Tometchy <tometchy@gmail.com>
2022-05-16 23:55:41 +0200

    Fix send function

--- _functions/send.js
+++ _functions/send.js
@@ -1,118 +1,16 @@
 const fetch = require("node-fetch");
 const atob = require("atob");
-
 const SENDGRID_API_ENDPOINT = "https://api.sendgrid.com/v3/mail/send";
 
-function checkStatus(res, propagateFailure) {
-    if (res.ok) { // res.status >= 200 && res.status < 300
-        return res;
-    } else {
-        console.error("Response status is not ok");
-        console.error(res);
-
-        if (propagateFailure) {
-            console.log("Propagating failure from wrong response status to user");
-            callback(new Error("Third party service doesn't work, could not send"));
-        }
-
-        return res;
-    }
-}
-
-function informUs(propagateFailure) {
-    var informUsBody = {
-        personalizations: [{ to: [{ email: "kontakt@gitwarsztaty.pl" }] }],
-        from: { email: eventBody.email },
-        subject: "Nowy request o " + eventBody.requestPurpose + " od " + eventBody.email,
-        content: [{
-            type: "text/plain",
-            value: "Nowy request o " + eventBody.requestPurpose + " od " + eventBody.email + "\n\n" +
-                "Czy zgodził się na Git w Twojej skrzynce: " + eventBody.agreeGitInbox + "\n\n" +
-                "Czy zgodził się na GitWarsztaty w Twojej skrzynce: " + eventBody.agreeGitWarsztatyInbox + "\n\n" +
-                "Czas: " + eventBody.time + "\n\n" +
-                "Url: " + eventBody.cameFromUrl + "\n\n" +
-                "Url (Jekyll): " + eventBody.cameFromUrlJekyll + "\n\n" +
-                "Który formularz uzupełnił: " + eventBody.cameFromFormLocation + "\n\n" +
-                "Czy w firmie: " + eventBody.inCompany + "\n\n" +
-                "Numer telefonu: " + atob(decodeURIComponent(escape(eventBody.phoneNumber))) + "\n\n" +
-                "(JEZELI W FIRMIE) Nazwa firmy: " + atob(decodeURIComponent(escape(eventBody.companyName))) + "\n\n" +
-                "(JEZELI W FIRMIE) Adres biura: " + atob(decodeURIComponent(escape(eventBody.officeAddress))) + "\n\n" +
-                "(JEZELI OTWARTE) Kto: " + atob(decodeURIComponent(escape(eventBody.who))) + "\n\n" +
-                "(JEZELI OTWARTE) Gdzie: " + atob(decodeURIComponent(escape(eventBody.whichCity))) + "\n\n" +
-                "Dodatkowe informacje: " + atob(decodeURIComponent(escape(eventBody.additionalInfo))) + "\n\n"
-        }]
-    };
-
-    informUsBody = JSON.stringify(informUsBody);
-    console.log("Sending inform us mail using sendgrid: " + informUsBody);
-
-    fetch(SENDGRID_API_ENDPOINT, {
-        method: 'post',
-        body: informUsBody,
-        headers: {
-            "Accept": "application/json",
-            'Content-Type': 'application/json',
-            'Authorization': ('Bearer ' + SENDGRID_API_KEY)
-        }
-    })
-        .then(res => checkStatus(res, propagateFailure))
-        .then(response => {
-            console.log("Got response for informing us email:");
-            console.log(response)
-        })
-        .catch(error => {
-            console.log("Error catched during informing us email");
-            console.error(error);
-
-            if (propagateFailure) {
-                console.log("Propagating inform us failure to user");
-                callback(new Error("Third party service doesn't work, could not send"));
-            }
-        });
-}
-
-function sendMaterialsWithSendgrid() {
-    var sendMaterialsBody = {
-        personalizations: [{ to: [{ email: eventBody.email }] }],
-        from: { email: "kontakt@gitwarsztaty.pl", name: "GitWarsztaty" },
-        subject: "11 Git Tricków do codziennej pracy",
-        content: [{
-            type: "text/html",
-            value: "<div>Cześć,<div><br>oto obiecany darmowy pdf - <a href=\"https://www.gitwarsztaty.pl/materialy/11-git-trickow-do-codziennej-pracy-gitwarsztatypl.pdf\">11 Git Tricków do codziennej pracy</a>.<br><br>Która porada spodobała Ci się najbardziej?</div><div>Zachęcamy do podzielenia się feedbackiem, wystarczy, że odpiszesz na tego maila.<br><br>Możesz od razu zacząć stosować nowe metody w praktyce... no może prawie od razu - najpierw prześlij je dalej pozostałym w Twojej firmie.</div><div><br>W końcu <b>razem tworzycie mocny zespół</b> 💪<br><br><i>Pozdrów zespół<br>Tomasz Skraskowski<br><a href=\"https://www.gitwarsztaty.pl\">GitWarsztaty.pl</a></i><br></div></div>"
-        }]
-    };
-
-    sendMaterialsBody = JSON.stringify(sendMaterialsBody);
-    console.log("Sending materials using sendgrid: " + sendMaterialsBody);
-
-    fetch(SENDGRID_API_ENDPOINT, {
-        method: 'post',
-        body: sendMaterialsBody,
-        headers: { "Accept": "application/json", 'Content-Type': 'application/json', 'Authorization': ('Bearer ' + SENDGRID_API_KEY) }
-    })
-        .then(res => checkStatus(res, true))
-        .then(response => {
-            console.log("Got response from sending materials with sendgrid:");
-            console.log(response)
-        })
-        .catch(error => {
-            console.log("Error catched during sending materials with sendgrid, propagating failure to user");
-            console.error(error);
-            callback(new Error("Third party service doesn't work, could not send"));
-        });
-}
-
 exports.handler = async (event, context) => {
+    const SENDGRID_API_KEY = process.env.SENDGRID;
+    const eventBody = JSON.parse(event.body);
+
     try {
         console.log('------------- REQUEST STARTED -------------------- VER 2020_05_05__18_39');
         console.log('event http method: ' + event.httpMethod);
         console.log('event body: ' + event.body);
-
-        const SENDGRID_API_KEY = process.env.SENDGRID;
         console.log("SendGrid api key: " + SENDGRID_API_KEY.substring(0, 4) + "...");
-
-        const eventBody = JSON.parse(event.body);
-
         console.log("email: " + eventBody.email);
         console.log("agreeGitInbox: " + eventBody.agreeGitInbox);
         console.log("agreeGitWarsztatyInbox : " + eventBody.agreeGitWarsztatyInbox);
@@ -159,5 +57,104 @@ exports.handler = async (event, context) => {
             statusCode: 500,
             body: JSON.stringify({ error: 'Failure' }),
         };
+    }
+
+    function checkStatus(res, propagateFailure) {
+        if (res.ok) { // res.status >= 200 && res.status < 300
+            return res;
+        } else {
+            console.error("Response status is not ok");
+            console.error(res);
+
+            if (propagateFailure) {
+                console.log("Propagating failure from wrong response status to user");
+                callback(new Error("Third party service doesn't work, could not send"));
+            }
+
+            return res;
+        }
+    }
+
+    function informUs(propagateFailure) {
+        var informUsBody = {
+            personalizations: [{ to: [{ email: "kontakt@gitwarsztaty.pl" }] }],
+            from: { email: eventBody.email },
+            subject: "Nowy request o " + eventBody.requestPurpose + " od " + eventBody.email,
+            content: [{
+                type: "text/plain",
+                value: "Nowy request o " + eventBody.requestPurpose + " od " + eventBody.email + "\n\n" +
+                    "Czy zgodził się na Git w Twojej skrzynce: " + eventBody.agreeGitInbox + "\n\n" +
+                    "Czy zgodził się na GitWarsztaty w Twojej skrzynce: " + eventBody.agreeGitWarsztatyInbox + "\n\n" +
+                    "Czas: " + eventBody.time + "\n\n" +
+                    "Url: " + eventBody.cameFromUrl + "\n\n" +
+                    "Url (Jekyll): " + eventBody.cameFromUrlJekyll + "\n\n" +
+                    "Który formularz uzupełnił: " + eventBody.cameFromFormLocation + "\n\n" +
+                    "Czy w firmie: " + eventBody.inCompany + "\n\n" +
+                    "Numer telefonu: " + atob(decodeURIComponent(escape(eventBody.phoneNumber))) + "\n\n" +
+                    "(JEZELI W FIRMIE) Nazwa firmy: " + atob(decodeURIComponent(escape(eventBody.companyName))) + "\n\n" +
+                    "(JEZELI W FIRMIE) Adres biura: " + atob(decodeURIComponent(escape(eventBody.officeAddress))) + "\n\n" +
+                    "(JEZELI OTWARTE) Kto: " + atob(decodeURIComponent(escape(eventBody.who))) + "\n\n" +
+                    "(JEZELI OTWARTE) Gdzie: " + atob(decodeURIComponent(escape(eventBody.whichCity))) + "\n\n" +
+                    "Dodatkowe informacje: " + atob(decodeURIComponent(escape(eventBody.additionalInfo))) + "\n\n"
+            }]
+        };
+
+        informUsBody = JSON.stringify(informUsBody);
+        console.log("Sending inform us mail using sendgrid: " + informUsBody);
+
+        fetch(SENDGRID_API_ENDPOINT, {
+            method: 'post',
+            body: informUsBody,
+            headers: {
+                "Accept": "application/json",
+                'Content-Type': 'application/json',
+                'Authorization': ('Bearer ' + SENDGRID_API_KEY)
+            }
+        })
+            .then(res => checkStatus(res, propagateFailure))
+            .then(response => {
+                console.log("Got response for informing us email:");
+                console.log(response)
+            })
+            .catch(error => {
+                console.log("Error catched during informing us email");
+                console.error(error);
+
+                if (propagateFailure) {
+                    console.log("Propagating inform us failure to user");
+                    callback(new Error("Third party service doesn't work, could not send"));
+                }
+            });
+    }
+
+    function sendMaterialsWithSendgrid() {
+        var sendMaterialsBody = {
+            personalizations: [{ to: [{ email: eventBody.email }] }],
+            from: { email: "kontakt@gitwarsztaty.pl", name: "GitWarsztaty" },
+            subject: "11 Git Tricków do codziennej pracy",
+            content: [{
+                type: "text/html",
+                value: "<div>Cześć,<div><br>oto obiecany darmowy pdf - <a href=\"https://www.gitwarsztaty.pl/materialy/11-git-trickow-do-codziennej-pracy-gitwarsztatypl.pdf\">11 Git Tricków do codziennej pracy</a>.<br><br>Która porada spodobała Ci się najbardziej?</div><div>Zachęcamy do podzielenia się feedbackiem, wystarczy, że odpiszesz na tego maila.<br><br>Możesz od razu zacząć stosować nowe metody w praktyce... no może prawie od razu - najpierw prześlij je dalej pozostałym w Twojej firmie.</div><div><br>W końcu <b>razem tworzycie mocny zespół</b> 💪<br><br><i>Pozdrów zespół<br>Tomasz Skraskowski<br><a href=\"https://www.gitwarsztaty.pl\">GitWarsztaty.pl</a></i><br></div></div>"
+            }]
+        };
+
+        sendMaterialsBody = JSON.stringify(sendMaterialsBody);
+        console.log("Sending materials using sendgrid: " + sendMaterialsBody);
+
+        fetch(SENDGRID_API_ENDPOINT, {
+            method: 'post',
+            body: sendMaterialsBody,
+            headers: { "Accept": "application/json", 'Content-Type': 'application/json', 'Authorization': ('Bearer ' + SENDGRID_API_KEY) }
+        })
+            .then(res => checkStatus(res, true))
+            .then(response => {
+                console.log("Got response from sending materials with sendgrid:");
+                console.log(response)
+            })
+            .catch(error => {
+                console.log("Error catched during sending materials with sendgrid, propagating failure to user");
+                console.error(error);
+                callback(new Error("Third party service doesn't work, could not send"));
+            });
     }
 };
