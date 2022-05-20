3fc4d673f84d7c8988dfb04a8cec76a54fefc317
Tometchy <tometchy@gmail.com>
2022-05-20 11:17:16 +0200

    Adjust materials mail text

--- _functions/send.js
+++ _functions/send.js
@@ -187,7 +187,7 @@ exports.handler = async (event, context) => {
             subject: "11 Git Tricków do codziennej pracy",
             content: [{
                 type: "text/html",
-                value: "<div>Cześć,<div><br>przesyłam link do obiecanego pdfa - <a href=\"https://www.gitwarsztaty.pl/materialy/11-git-trickow-do-codziennej-pracy-gitwarsztatypl.pdf\">11 Git Tricków do codziennej pracy</a>.<br><br>Która porada spodobała Ci się najbardziej?</div><div>Zachęcam do podzielenia się feedbackiem, wystarczy, że odpiszesz mi na tego maila :)<br><br>Możesz od razu zacząć stosować nowe metody w praktyce... no może prawie od razu - najpierw prześlij je dalej pozostałym w Twojej firmie.</div><div><br>W końcu <b>razem tworzycie mocny zespół</b> 💪<br><br><i>Pozdrów zespół<br>Tomasz Skraskowski<br><a href=\"https://www.gitwarsztaty.pl\">GitWarsztaty.pl</a></i><br></div></div>"
+                value: "<div>Cześć,<div><br>przesyłam link do obiecanego pdfa - <a href=\"https://www.gitwarsztaty.pl/materialy/11-git-trickow-do-codziennej-pracy-gitwarsztatypl.pdf\">11 Git Tricków do codziennej pracy</a>.<br><br>Która porada spodobała Ci się najbardziej?</div><div>Zachęcam do podzielenia się feedbackiem, wystarczy, że odpiszesz mi na tego maila :)<br><br>Możesz od razu zacząć stosować nowe metody w praktyce, a jeśli są dla Ciebie wartościowe, prześlij je dalej pozostałym w Twojej firmie.</div><div><br>W końcu <b>razem tworzycie mocny zespół</b> 💪<br><br><i>Pozdrów zespół<br>Tomasz Skraskowski<br><a href=\"https://www.gitwarsztaty.pl\">GitWarsztaty.pl</a></i><br></div></div>"
             }]
         };
 
